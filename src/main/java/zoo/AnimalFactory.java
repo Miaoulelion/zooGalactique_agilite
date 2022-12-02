8306e923277db5c761871fa2c42318f39d027ac8
SylvainVitry <vitrysylvain@orange.fr>
2022-12-02 10:28:10 +0100

    Modif de la factory en generique

--- src/main/java/zoo/AnimalFactory.java
+++ src/main/java/zoo/AnimalFactory.java
@@ -2,27 +2,27 @@ package zoo;
 
 public class AnimalFactory {
 
-	public static Lion getLion(Zoo zoo, String nom, float poids, int taille) {
+	public static Animal getLion(Zoo zoo, String nom, float poids, int taille) {
 		return new Lion(zoo, nom, poids, taille);
 	}
 	
-	public static Lion getLion() {
+	public static Animal getLion() {
 		return new Lion();
 	}
 	
-	public static Tigre getTigre(Zoo zoo, String nom, float poids, int taille) {
+	public static Animal getTigre(Zoo zoo, String nom, float poids, int taille) {
 		return new Tigre(zoo, nom, poids, taille);
 	}
 	
-	public static Tigre getTigre() {
+	public static Animal getTigre() {
 		return new Tigre();
 	}
 	
-	public static Singe getSinge(Zoo zoo, String nom, float poids, int taille) {
+	public static Animal getSinge(Zoo zoo, String nom, float poids, int taille) {
 		return new Singe(zoo, nom, poids, taille);
 	}
 	
-	public static Singe getSinge() {
+	public static Animal getSinge() {
 		return new Singe();
 	}
 }
