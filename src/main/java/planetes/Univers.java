7c0e4ef81b0641b9ceb0d3e982033e77dbab1b66
Miaoulelion <nicolas-berthier@orange.fr>
2022-12-07 14:21:28 +0100

    assurance multithread pour univers

--- src/main/java/planetes/Univers.java
+++ src/main/java/planetes/Univers.java
@@ -3,9 +3,14 @@ package planetes;
 import java.util.LinkedList;
 
 public final class Univers {
+	private static final Univers INSTANCE = new Univers();
 	private static LinkedList<Planete> planetes = new LinkedList<Planete>();
 	
 	private Univers() {
+	}
+	
+	public static Univers getInstance() {
+		return INSTANCE;
 	}
 	
 	public static void addPlanete(Planete planete) {
